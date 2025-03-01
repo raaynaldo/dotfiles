4d7fc3715ab586a0187b5fcfadf373522fe17dea
Raynaldo Sutisna <raaynaldo@gmail.com>
2025-03-01 12:28:49 -0500

    feat: create different script

---
+++ run_once_install-omz-plugins-theme.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+echo "Installing oh-my-zsh plugins and spaceship theme"
+brew install autojump spaceship zsh-autocomplete zsh-autosuggestions zsh-syntax-highlighting
--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -2,10 +2,7 @@
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Getting ohmyz.sh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-  echo "Installing oh-my-zsh plugins and spaceship theme"
-  brew install autojump spaceship zsh-autocomplete zsh-autosuggestions zsh-syntax-highlighting
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc:
 else
   echo ".oh-my-zsh already found, skipping."
 fi
