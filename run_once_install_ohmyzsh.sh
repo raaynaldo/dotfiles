8d74052e57f75c7187c120059aca25ad89427ec1
Raynaldo Sutisna <raaynaldo@gmail.com>
2025-03-01 12:18:07 -0500

    feat: update script

--- run_once_install_ohmyzsh.sh
+++ run_once_install_ohmyzsh.sh
@@ -2,7 +2,9 @@
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Getting ohmyz.sh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" ""
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  echo "Installing oh-my-zsh plugins and spaceship theme"
   brew install autojump spaceship zsh-autocomplete zsh-autosuggestions zsh-syntax-highlighting
 else
   echo ".oh-my-zsh already found, skipping."
